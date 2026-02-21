1f905ad521a737665295e023749f805a2f3a428a
xmdhkdltmvos2032 <85145889+xmdhkdltmvos2032@users.noreply.github.com>
2026-02-21 15:01:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,9 +4,17 @@ from googlenewsdecoder import gnewsdecoder
 app = FastAPI()
 
 @app.get("/decode")
-def decode(url: str):
+def decode(url: str, title: str):
     try:
-        real_url = gnewsdecoder(url)
-        return {"real_url": real_url}
+        result = gnewsdecoder(url)
+
+        if not result.get("status"):
+            return {"error": "Decoding failed"}
+
+        return {
+            "title": title,
+            "decoded_url": result["decoded_url"]
+        }
+
     except Exception as e:
         return {"error": str(e)}
